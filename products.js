74187a76a869f71b7ff084d46376fe1a6879989f
navidmsv <navidmsv@icloud.com>
2026-09-10 19:24:23 +0330

    Create products.js

---
+++ products.js
@@ -0,0 +1,41 @@
+const products = [
+  {
+    id: 1,
+    name: "حوله حمام کلاسیک",
+    price: 890000,
+    oldPrice: 1090000,
+    badge: "پرفروش",
+    category: "bath",
+    image: "images/bath-towel.webp"
+  },
+
+  {
+    id: 2,
+    name: "حوله دست و صورت",
+    price: 390000,
+    oldPrice: 450000,
+    badge: "محبوب",
+    category: "hand",
+    image: "images/hand-towel.webp"
+  },
+
+  {
+    id: 3,
+    name: "حوله استخری",
+    price: 690000,
+    oldPrice: 790000,
+    badge: "جدید",
+    category: "pool",
+    image: "images/pool-towel.webp"
+  },
+
+  {
+    id: 4,
+    name: "ست حوله دو نفره",
+    price: 1490000,
+    oldPrice: 1690000,
+    badge: "ویژه",
+    category: "set",
+    image: "images/towel-set.webp"
+  }
+];
