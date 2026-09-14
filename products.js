f9249800c91546a1e3ea9c80a535cc558dfc1953
navidmsv <navidmsv@icloud.com>
2026-09-15 02:59:24 +0330

    ویرایش محصول #1

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products =
     {
         id: 1,
         name: "حوله حمام لوکس آریا",
-        price: 55555555,
+        price: 0,
         oldPrice: 1590000,
         badge: "پرفروش",
         categories: ["bath", "men", "women"],
