7a15a63205978dbd6a088fab2ae9ec3f27928fda
navidmsv <navidmsv@icloud.com>
2026-09-14 16:38:05 +0330

    ویرایش محصول #1

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products =
         price: 1290000,
         oldPrice: 1590000,
         badge: "پرفروش",
-        categories: ["bath", "men", "women"],
+        categories: ["bath", "men", "women"], "men", "women"],
         image: "images/bath-luxury-aria.webp",
         images: [
             "images/bath-luxury-aria.webp",
