eaadd63a14bf5f5caa164c9526f124df1518ac36
navidmsv <navidmsv@icloud.com>
2026-09-15 03:02:24 +0330

    ویرایش محصول #1

--- products.js
+++ products.js
@@ -12,7 +12,7 @@ const products =
             "images/bath-luxury-aria-2.webp",
             "images/bath-luxury-aria-3.webp"
         ],
-        colors: [],
+        colors: [{ name: "سفید", stock: 3 }],
         description: "حوله حمام نرم و باکیفیت با جذب آب بالا، مناسب استفاده روزمره.",
         quality: "درجه یک",
         material: "پنبه",
