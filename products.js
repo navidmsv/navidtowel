cdb2ad93370235b1775b86cd28f27e15fe5e91a5
navidmsv <navidmsv@icloud.com>
2026-09-17 17:59:07 +0330

    Update products.js

--- products.js
+++ products.js
@@ -12,7 +12,7 @@ const products =
             "images/products/product-1-قرمز-1789652935511.jpg",
             "images/bath-luxury-aria-3.webp"
         ],
-        colors: [{ name: "ابی", stock: 3," }, { name: "قرمز", stock: 5," }],
+        colors: [{ name: "ابی", stock:3}, { name: "قرمز", stock:5}],
         description: "حوله حمام نرم و باکیفیت با جذب آب بالا، مناسب استفاده روزمره.",
         quality: "درجه یک",
         material: "پنبه",
